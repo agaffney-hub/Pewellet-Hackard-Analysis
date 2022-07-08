e60bffe8ec6c76c21de417141f39f7e614588806
agaffney-hub <amyjonaye@gmail.com>
2022-07-07 18:01:02 -0700

    Added charts and connected references in additional charts.

--- schema_seven.sql
+++ schema_seven.sql
@@ -31,3 +31,21 @@ CREATE TABLE salaries (
   FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
   PRIMARY KEY (emp_no)
 );
+CREATE TABLE dept_employees (
+dept_no VARCHAR(4) NOT NULL,
+    emp_no INT NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+    PRIMARY KEY (emp_no, dept_no)
+);
+CREATE TABLE titles (
+    emp_no INT NOT NULL,
+	title VARCHAR NOT NULL,
+    from_date DATE NOT NULL,
+    to_date DATE NOT NULL,
+FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
+    PRIMARY KEY (emp_no)
+);
+SELECT * FROM departments;
